500ce0e6c6f400227bbbe21678d38f2472071192
haider0107 <kushal.git01@gmail.com>
2024-02-05 19:07:30 +0530

    New File added

---
+++ Organisation/BLL/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace BLL
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+
+        public double SalPerDay { get; set; }
+
+        public static double HRA { get; set; } = 12.5;
+
+        public double CalculateSalary(int NoOfWorkingDays)
+        {
+            var totalSalary = (SalPerDay * NoOfWorkingDays) + (SalPerDay * NoOfWorkingDays) * HRA/100;
+            return totalSalary;
+        }
+    }
+}
---
+++ Organisation/UIConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using BLL;
+
+Employee employee = new Employee() { Id = 1, Name = "Anurag", SalPerDay = 23.5 };
+
+Console.WriteLine("Enter number of working days...");
+int n = int.Parse(Console.ReadLine());
+
+var Salary = employee.CalculateSalary(n);
+
+Console.WriteLine($"Eid: {employee.Id} Employee Name: {employee.Name} Salary Per Day: {employee.SalPerDay} Total Salary: {Salary}");
+
+Console.ReadLine();
