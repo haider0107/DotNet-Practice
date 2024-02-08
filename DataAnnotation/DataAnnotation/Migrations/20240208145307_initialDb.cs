1a6c3d88bb35d583d512d3d05f8540db29e0841b
haider0107 <kushal.git01@gmail.com>
2024-02-08 22:01:45 +0530

    EF core basics

---
+++ DataAnnotation/DataAnnotation/Migrations/20240208145307_initialDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAnnotation.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: true),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Salary = table.Column<double>(type: "float", nullable: false),
+                    DOJ = table.Column<DateTime>(type: "Date", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LkInProfileLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Did = table.Column<int>(type: "int", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Eid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ DataAnnotation/DataAnnotation/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello World");
+
+var db = new OrgDBContext();
+//Select All
+var emps = db.Employees.ToList();
+//var emps = (from res in db.Employees select res).ToList();
+foreach (var e in emps)
+{
+    Console.WriteLine($"Eid:{e.Eid} Ename:{e.EName} ");
+}
+
+//Select Employee With id=2
+//var e = db.Employees.Find(2);
+//Console.WriteLine($"Eid:{e.Eid} Ename:{e.EName} Gender:{e.Gender} CreatedOn:{e.CreatedOn}");
+
+//Select employee name=peter
+//var e = db.Employees.Where(x => x.EName == "Peter").FirstOrDefault();
+//Console.WriteLine($"Eid:{e.Eid} Ename:{e.EName} Gender:{e.Gender} CreatedOn:{e.CreatedOn}");
+
+
+//Update emp 2 Did to 1000
+//var e = db.Employees.Find(2);
+//e.Did = 1000;
+//db.Employees.Update(e);
+//db.SaveChanges();
+
+//Delete emp 2
+//var e = db.Employees.Find(2);
+//db.Employees.Remove(e);
+//db.SaveChanges();
+
+//var e = new Employee()
+//{
+//    Eid = 3,
+//    EName = "Lilly",
+//    Gender = "F",
+//    Email = "lilly@gmail.com",
+//    ConfirmEmail = "lilly@gmail.com",
+//    Salary = 70000,
+//    DOJ = new DateTime(2010, 3, 10),
+//    DOB=new DateTime(1984,11,6),
+//    Did=1000,
+//    LkInProfileLink="https://wwww.Manzoorthetrainer.com"
+//};
+
+//db.Employees.Add(e);
+//db.SaveChanges();
+
+[Table("Employee")]
+public class Employee
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int Eid { get; set; }
+
+    [Required]
+    [Column("EmployeeName")]
+    public string? EName { get; set; }
+
+    [MaxLength(2)]
+    public string? Gender { get; set; }
+
+    [Column(TypeName = "varchar(50)")]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [NotMapped]  // will not store in database
+    [Compare("Email")]
+    public string? ConfirmEmail { get; set; }
+
+    [Range(0, 100000)]
+    public double Salary { get; set; }
+
+    [Column(TypeName = "Date")]
+    public DateTime DOJ { get; set; }
+
+    public DateTime DOB { get; set; }
+
+    [Url]
+    public string? LkInProfileLink { get; set; }
+    public int? Did { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
+}
+
+public class OrgDBContext:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("postgres://tlocyvts:xvJZsRIYNIqkg-TtVxxjqUM8rPw6aB68@berry.db.elephantsql.com/tlocyvts");
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
